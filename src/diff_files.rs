da3150e8f15827c5a26919b578e8614e6fd96fc8
Taco <87836366+Tacotakedown@users.noreply.github.com>
2024-04-15 23:30:09 -0600

    fix parallelism

--- src/diff_files.rs
+++ src/diff_files.rs
@@ -28,69 +28,11 @@ fn diff_files(file_a: &str, file_b: &str) -> Vec<u8> {
     patch
 }
 
-// diffs all files that are changed and generates a .download file containing the bytes need to patch every file
-pub async fn dif_from_map(
-    map: PatchMapStructure,
-    aircraft_folder_a: Arc<String>,
-    aircraft_folder_b: Arc<String>,
-    output_path: &str,
-) -> std::io::Result<()> {
-    let map_arc = Arc::new(map);
-    let mut combined_data = vec![Vec::new(); map_arc.changed_files.len()];
-
-    for (index, relative_path) in map_arc.changed_files.iter().enumerate() {
-        let file_path_a = format!("{}/{}", aircraft_folder_a, relative_path);
-        let file_path_b = format!("{}/{}", aircraft_folder_b, relative_path);
-
-        if let Err(err) = fs::metadata(&file_path_a) {
-            eprintln!(
-                "Error model A: {} does not exist or is inaccessible: {}",
-                file_path_a, err
-            );
-            continue; // Skip this file and move to the next one
-        }
-        if let Err(err) = fs::metadata(&file_path_b) {
-            eprintln!(
-                "Error model B: {} does not exist or is inaccessible: {}",
-                file_path_a, err
-            );
-            continue; // Skip this file and move to the next one
-        }
-
-        let handle = spawn_task_async(
-            Arc::clone(&map_arc),
-            Arc::clone(&aircraft_folder_a),
-            Arc::clone(&aircraft_folder_b),
-            relative_path.clone(),
-            index,
-            &mut combined_data,
-        );
-
-        handle.await?;
-    }
-
-    let output_file_path = format!("{}/combined.bin", output_path);
-    let mut output_file = OpenOptions::new()
-        .write(true)
-        .create(true)
-        .truncate(true)
-        .open(output_file_path)?;
-
-    for data in &combined_data {
-        output_file.write_all(data)?;
-    }
-
-    Ok(())
-}
-
 async fn spawn_task_async(
-    map_arc: Arc<PatchMapStructure>,
     aircraft_folder_a: Arc<String>,
     aircraft_folder_b: Arc<String>,
     relative_path: String,
-    index: usize,
-    combined_data: &mut Vec<Vec<u8>>,
-) -> std::io::Result<()> {
+) -> Vec<u8> {
     let file_path_a = format!("{}/{}", aircraft_folder_a, relative_path);
     let file_path_b = format!("{}/{}", aircraft_folder_b, relative_path);
 
@@ -100,7 +42,49 @@ async fn spawn_task_async(
 
     println!("Time taken for {}: {:?}", relative_path, elapsed_time);
 
-    combined_data[index] = diff_result;
+    diff_result
+}
+
+// diffs all files that are changed and generates a .download file containing the bytes need to patch every file
+pub async fn dif_from_map(
+    map: PatchMapStructure,
+    aircraft_folder_a: Arc<String>,
+    aircraft_folder_b: Arc<String>,
+    output_path: &str,
+) -> std::io::Result<()> {
+    let map_arc = Arc::new(map);
+    let mut handles = vec![];
+    let mut combined_data = vec![Vec::new(); map_arc.changed_files.len()];
+
+    for (index, relative_path) in map_arc.changed_files.iter().enumerate() {
+        let aircraft_folder_a_clone = Arc::clone(&aircraft_folder_a);
+        let aircraft_folder_b_clone = Arc::clone(&aircraft_folder_b);
+        let relative_path_clone = relative_path.clone();
+
+        let handle = task::spawn(async move {
+            let diff_result = spawn_task_async(
+                aircraft_folder_a_clone,
+                aircraft_folder_b_clone,
+                relative_path_clone,
+            )
+            .await;
+            (index, diff_result)
+        });
+
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        let (index, diff_result) = handle.await?;
+        combined_data[index] = diff_result;
+    }
+
+    let output_file_path = format!("{}/combined.bin", output_path);
+    let mut output_file = File::create(output_file_path)?;
+
+    for data in &combined_data {
+        output_file.write_all(data)?;
+    }
 
     Ok(())
 }
