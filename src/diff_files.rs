3f5353b43839f4168dfbd692025d9fa9b96b8019
Taco <87836366+Tacotakedown@users.noreply.github.com>
2024-04-17 01:16:43 -0600

    refactor diffing

--- src/diff_files.rs
+++ src/diff_files.rs
@@ -1,7 +1,7 @@
 use crate::dds_differ::create_diff;
 use bidiff::simple_diff as bdiff;
 use std::fs::OpenOptions;
-use std::io::Write;
+use std::io::{self, Write};
 use std::sync::{Arc, Mutex};
 use std::time::{Duration, Instant};
 use std::{
@@ -42,74 +42,58 @@ fn diff_files(file_a: &str, file_b: &str) -> Vec<u8> {
 async fn spawn_task_async(
     aircraft_folder_a: Arc<String>,
     aircraft_folder_b: Arc<String>,
+    output_path: String,
     relative_path: String,
-) -> Vec<u8> {
+) -> io::Result<()> {
     let file_path_a = format!("{}/{}", aircraft_folder_a, relative_path);
     let file_path_b = format!("{}/{}", aircraft_folder_b, relative_path);
 
     let start_time = Instant::now();
-    let diff_result = diff_files(&file_path_a, &file_path_b);
+
+    hdiff_rs::diff_files(&file_path_a, &file_path_b, &output_path).expect("error diffing");
     let elapsed_time = start_time.elapsed();
 
     println!("Time taken for {}: {:?}", relative_path, elapsed_time);
 
-    diff_result
+    Ok(())
 }
 
 // diffs all files that are changed and generates a .download file containing the bytes need to patch every file
-pub async fn dif_from_map(
+pub async fn diff_from_map(
     map: PatchMapStructure,
     aircraft_folder_a: Arc<String>,
     aircraft_folder_b: Arc<String>,
-    output_path: &str,
+    output_path: String,
 ) -> std::io::Result<()> {
     let map_arc = Arc::new(map);
     let mut handles = vec![];
-    let mut combined_data = vec![Vec::new(); map_arc.changed_files.len()];
 
-    for (index, relative_path) in map_arc.changed_files.iter().enumerate() {
+    for relative_path in map_arc.changed_files.iter() {
         let aircraft_folder_a_clone = Arc::clone(&aircraft_folder_a);
         let aircraft_folder_b_clone = Arc::clone(&aircraft_folder_b);
         let relative_path_clone = relative_path.clone();
+        let output_path_clone = output_path.clone();
 
         let handle = task::spawn(async move {
-            let diff_result = spawn_task_async(
+            let res = spawn_task_async(
                 aircraft_folder_a_clone,
                 aircraft_folder_b_clone,
+                output_path_clone,
                 relative_path_clone,
             )
             .await;
-            (index, diff_result)
+
+            match res {
+                Ok(()) => {}
+
+                Err(e) => {
+                    eprintln!("failed to call fn in thread {}", e)
+                }
+            }
         });
 
         handles.push(handle);
     }
-
-    for handle in handles {
-        let (index, diff_result) = handle.await?;
-        combined_data[index] = diff_result;
-    }
-
-    let output_file_path = format!("{}/combined.bin", output_path);
-    let mut output_file = File::create(output_file_path.clone())?;
-
-    for data in &combined_data {
-        output_file.write_all(data)?;
-    }
-
-    let zip_path = format!("{}/combined.zip", output_path);
-    let zip_file = File::create(&zip_path)?;
-    let mut writer = ZipWriter::new(zip_file);
-
-    let options = FileOptions::default()
-        .compression_method(CompressionMethod::Zstd)
-        .unix_permissions(0o755);
-
-    writer.start_file("combined.bin", options)?;
-    let mut output_file = File::open(&output_file_path)?;
-    std::io::copy(&mut output_file, &mut writer)?;
-
-    writer.finish()?;
 
     Ok(())
 }
